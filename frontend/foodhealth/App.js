4a43a5b8095e128ca7ef94b37023dabf33bcfdbf
yves <jeanpetityvelos@gmail.com>
2022-12-03 02:23:06 +0100

    ajout du composant home dans le frontend

--- frontend/foodhealth/App.js
+++ frontend/foodhealth/App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +11,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative',
+    width: '563px',
+    height: '943px',
+    backgroundColor: 'linear-gradient(191.65deg, rgba(15, 86, 224, 0.66) 8.54%, #A0BCF3 99.15%)'
   },
 });
